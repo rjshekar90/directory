55f1000063e75d57c3dfeb8b27fcec3b90a27a65
Raj <rjshekar90@gmail.com>
2017-09-11 15:44:17 -0400

    DB is created

--- www/models.py
+++ www/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length = 40)
+    email = models.CharField(max_length = 50)
+    title = models.CharField(max_length = 50)
+    image = models.CharField(max_length = 200)
