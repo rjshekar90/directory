853cbfa7e931e79411395b35ff42286f8aecb5da
Raj <rjshekar90@gmail.com>
2017-09-13 21:44:32 -0400

    Minor commits

--- www/models.py
+++ www/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -7,6 +8,15 @@ class Person(models.Model):
     email = models.CharField(max_length = 50)
     title = models.CharField(max_length = 50)
     #image = models.CharField(max_length = 200)
+    slug  = models.SlugField(max_length = 40, blank = True)
+
+
+
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            slug = slugify(self.name)
+        super(Person, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
--- www/views.py
+++ www/views.py
@@ -5,3 +5,7 @@ from .models import Person
 def index(request):
     people = Person.objects.all()
     return render(request, "index.html", {"people": people})
+
+def detail(request, slug):
+    person = Person.objects.get(slug = slug)
+    return render(request, "detail.html", {"person": person})
